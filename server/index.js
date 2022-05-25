14e9855492a7f9cbef7cf7a1eae0bfdf3daaa1df
Pawior <pawiorx@gmail.com>
2022-05-25 17:16:28 +0200

    Working socket io and some api requests

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const app = express();
 require("dotenv").config();
 
 const PORT = process.env.APP_PORT;
-// const server = require("http").createServer();
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const httpServer = createServer(app);
@@ -26,47 +25,6 @@ const io = new Server(httpServer, {
   },
 });
 
-// io.on("connection", (socket) => {
-//   // ...
-// });
-
-// const io = require("socket.io")(server, {
-//   // ZAMIAST server - port ???
-//   cors: {
-//     origin: ["http://127.0.0.1:5500"],
-//   },
-// });
-
-const userIo = io.of("/user");
-userIo.on("connection", (socket) => {
-  console.log("Connected to user namespace with username " + socket.username);
-});
-
-userIo.use((socket, next) => {
-  if (socket.handshake.auth.token) {
-    socket.username = getUsernameFromToken(socket.handshake.auth.token);
-    next();
-  } else {
-    next(new Error("Couldn't connect to user namespace, please send token'"));
-  }
-});
-
-function getUsernameFromToken(token) {
-  return token;
-}
-
-// io.on("connection", (socket) => {
-//   console.log(socket.id);
-//   // socket.on("custom-event", (number, string, obj) => {
-//   //   console.log(number, string, obj);
-//   // });
-//   socket.on("send-message", (message) => {
-//     console.log(message);
-//     // io.emit("receive-message", message);
-//     socket.broadcast.emit("receive-message", message);
-//   });
-// });
-
 httpServer.listen(PORT, () => {
   console.log("Listening on port " + PORT);
 });
@@ -75,7 +33,6 @@ app.use("/", routerJS);
 
 const sendActivePiecesArr = require("./app/socketController");
 const onConnection = (socket) => {
-  console.log("coś");
   sendActivePiecesArr(io, socket);
 };
 
