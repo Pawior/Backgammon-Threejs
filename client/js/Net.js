056b1280e21406ae926b3da3e51665ff95646b16
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2022-06-11 11:37:47 +0200

    Co-authored-by: Wiktor Orda <wiktor.orda@gmail.com>

--- client/js/Net.js
+++ client/js/Net.js
@@ -3,7 +3,11 @@ import handleStateChange from "./net/handleStateChange.js";
 export default class Net {
   static async logIn(nick) {
     const body = JSON.stringify({ nick: nick });
-    const req = await fetch("/logIn", { method: "POST", body: body });
+    const req = await fetch("/logIn", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: body,
+    });
 
     const info = await req.json();
 
