48eafd024313b58a8ea33e651327855fbe763ebb
Pawior <pawiorx@gmail.com>
2022-06-13 19:57:52 +0200

    port changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-const PORT = process.env.APP_PORT;
+const PORT = process.env.PORT;
 const { createServer } = require("http");
 const httpServer = createServer(app);
 const routerJS = require("./app/router");
