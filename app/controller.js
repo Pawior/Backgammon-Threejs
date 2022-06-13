946fe5ad447e03de095342c1ac294a40c7788819
Pawior <pawiorx@gmail.com>
2022-06-13 19:40:36 +0200

    deploy version 1.0

---
+++ app/controller.js
@@ -0,0 +1,60 @@
+let { activePiecesArr } = require("./model");
+let { beatenPiecesArr } = require("./model");
+let { users, games } = require("./model");
+let path = require("path");
+
+module.exports = {
+  getHomePage: (req, res) => {
+    console.log(activePiecesArr);
+    console.log("halo");
+    console.log(__dirname);
+    // res.sendFile(path.join( __dirname, "views", "home.html"));
+    res.sendFile(path.join(__dirname, "..", "..", "client", "home.html"));
+  },
+  getActivePiecesArr: (req, res) => {
+    res.send(activePiecesArr);
+  },
+  getBeatenPiecesArr: (req, res) => {
+    res.send(beatenPiecesArr);
+  },
+  addActivePiece: (req, res) => {
+    console.log(req.body);
+    activePiecesArr.push(req.body);
+    res.send({ info: `dodaje${req.body}` });
+  },
+  addBeatenPiece: (req, res) => {
+    console.log(req.body);
+    beatenPiecesArr.push(req.body);
+    res.send({ info: `dodaje${req.body}` });
+  },
+  //
+  logIn: (req, res) => {
+    const nick = req.body.nick;
+    const color = users.length + 1;
+    users.push({ nick: nick, color: color });
+    console.log(users);
+    res.json({ color: color });
+  },
+  handleState: (req, res) => {
+    let state = req.body.state;
+    if (state === "waiting-for-opponent") {
+      res.send({ message: "showLoadingScreen" });
+    } else if (state === "your-turn") {
+      res.send({ message: "hideWaitingScreen" });
+    } else if (state === "opponents-turn") {
+      res.send({ message: "showWaitingScreen" });
+    } else if (state === "you-won") {
+      res.send({ message: "victory" });
+    } else if (state === "you-lost") {
+      res.send({ message: "lost" });
+    } else {
+      res.error({ message: "cant find that status" });
+    }
+  },
+  gamesHistory: (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "client", "gamesHistory.html"));
+  },
+  userStatPage: (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "client", "stats.html"));
+  },
+};
